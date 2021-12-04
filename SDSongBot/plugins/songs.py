44baca9c6b566a131a8c9833cc154801aabb65d7
Sanila Ranatunga <88930336+sanila2007@users.noreply.github.com>
2021-12-04 11:34:51 +0530

    Update songs.py

--- SDSongBot/plugins/songs.py
+++ SDSongBot/plugins/songs.py
@@ -62,7 +62,7 @@ def song(client, message):
         s = message.reply_audio(audio_file, caption=rep, thumb=thumb_name, parse_mode='md', title=title, duration=dur)
         m.delete()
     except Exception as e:
-        m.edit('❌ Error\nReport this problem👇/n @sanilaassistant_bot')
+        m.edit('❌ Error\nReport this problem👇\n@sanilaassistant_bot')
         print(e)
 
     try:
