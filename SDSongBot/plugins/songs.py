660a5453c62d0a4b6b20d1e9d7a3d1c014333a34
Sanila Ranatunga <88930336+sanila2007@users.noreply.github.com>
2021-12-26 15:56:30 +0530

    Update songs.py

--- SDSongBot/plugins/songs.py
+++ SDSongBot/plugins/songs.py
@@ -48,13 +48,13 @@ def song(client, message):
         )
         print(str(e))
         return
-    m.edit("✨️Your song is currently been downloading✨️")
+    m.edit("Downloading...✨️")
     try:
         with youtube_dl.YoutubeDL(ydl_opts) as ydl:
             info_dict = ydl.extract_info(link, download=False)
             audio_file = ydl.prepare_filename(info_dict)
             ydl.process_info(info_dict)
-        rep = '**~ Uploaded by @SanilaRanatunga✨️ ~**'
+        rep = '**~ Developer: Sanila Ranatunga✨️ ~**'
         secmul, dur, dur_arr = 1, 0, duration.split(':')
         for i in range(len(dur_arr)-1, -1, -1):
             dur += (int(dur_arr[i]) * secmul)
